1db04141a7639cb7c539d1be04ab1a4412606634
Vyacheslav Byrdaev <smit467bs@gmail.com>
2022-08-01 22:51:37 +0300

    feat

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  var brackets = {};
+  var stack = [];
+
+  if (str.length % 2 !== 0) return false;
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+
+    brackets[`${bracketsConfig[i][1]}`] = bracketsConfig[i][0];
+
+  }
+
+  for (let i =  0; i < str.length; i++) {
+
+    const curr = str[i];
+    const topElement = stack[stack.length - 1];
+    
+    if (brackets[curr] === topElement && stack.length > 0){
+
+      stack.pop();
+      
+    }
+      else{
+
+        stack.push(curr);
+      
+      }
+  }
+
+  return stack.length === 0;
 }
+  
+
+
+
